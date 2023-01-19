1fb9e06f7df1effa6d115e940b9162c506fb53a5
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-01-19 16:29:02 +0200

    update timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,12 +18,13 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+      if(selectedDates[0] <= new Date)
+      {alert("Please choose a date in the future")};
     },
   };
 
   // конвертер для вывода даты
-function convertMs(ms) {
+function convertMs(deltaDate) {
     
     const second = 1000;
     const minute = second * 60;
